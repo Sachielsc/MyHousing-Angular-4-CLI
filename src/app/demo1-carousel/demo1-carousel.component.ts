ed76266b3825d198fc7f0af5a6a85c89fc9a4b64
Sachielsc <sachielsc@gmail.com>
2017-10-02 11:22:41 +1300

    multiple modifications

--- src/app/demo1-carousel/demo1-carousel.component.ts
+++ src/app/demo1-carousel/demo1-carousel.component.ts
@@ -1,18 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-demo1-carousel',
   templateUrl: './demo1-carousel.component.html',
-  styleUrls: ['./demo1-carousel.component.css'],
-  providers: [NgbCarouselConfig]
+  styleUrls: ['./demo1-carousel.component.css']
 })
 export class Demo1CarouselComponent implements OnInit {
-  constructor(config: NgbCarouselConfig) {
-    // customize default values of carousels used by this component tree
-    config.interval = 10000;
-    config.wrap = false;
-    config.keyboard = false;
+  constructor() {
   }
   ngOnInit() {
   }
